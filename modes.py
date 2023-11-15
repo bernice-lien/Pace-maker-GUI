2a37dee24038463fec9ee98c48dade92a1809c07
Isaac Chen <cheni17@mcmaster.ca>
2023-11-15 17:16:31 -0500

    added enable buttons for m1 functions

--- modes.py
+++ modes.py
@@ -11,8 +11,10 @@ class modes():
         aai_middleframe = tk.Frame(aai, bg = '#4863A0')
         welcome_message = tk.Label(aai_middleframe, text = "AAI", bg='#4863A0', fg='#FFFFFF', font=("Arial", 16))
         backAAI = tk.Button(aai_middleframe, text = "Back", bg='#FFFFFF', fg='#000000', font=("Arial", 12), command = aai.destroy)
+        enableAAI = tk.Button(aai_middleframe, text = "Enable AAI", bg='#FFFFFF', fg='#000000', font=("Arial", 12) ) #implement serial enable 
         welcome_message.grid(row = 0, column = 0)
-        backAAI.grid(row = 1, column = 0)
+        enableAAI.grid(row = 1, column=0, pady=10)
+        backAAI.grid(row = 2, column = 0)
         aai_middleframe.pack()
         
         aai.mainloop()
@@ -25,23 +27,12 @@ class modes():
         aoo_middleframe = tk.Frame(aoo, bg = '#4863A0')
         welcome_message = tk.Label(aoo_middleframe, text = "AOO", bg='#4863A0', fg='#FFFFFF', font=("Arial", 16))
         backAOO = tk.Button(aoo_middleframe, text = "Back", bg='#FFFFFF', fg='#000000', font=("Arial", 12), command = aoo.destroy)
+        enableAOO = tk.Button(aoo_middleframe, text = "Enable AOO", bg='#FFFFFF', fg='#000000', font=("Arial", 12) ) #implement serial enable 
         welcome_message.grid(row = 0, column = 0)
-        backAOO.grid(row = 1, column = 0)
+        enableAOO.grid(row = 1, column=0, pady=10)
+        backAOO.grid(row = 2, column = 0)
         aoo_middleframe.pack()
         aoo.mainloop()
-    
-    def open_VVI(self):
-        vvi = tk.Tk()
-        vvi.geometry("500x400")
-        vvi.configure(bg='#4863A0')
-        vvi.title("VVI Page")
-        vvi_middleframe = tk.Frame(vvi, bg = '#4863A0')
-        welcome_message = tk.Label(vvi_middleframe, text = "VVI", bg='#4863A0', fg='#FFFFFF', font=("Arial", 16))
-        backVVI = tk.Button(vvi_middleframe, text = "Back", bg='#FFFFFF', fg='#000000', font=("Arial", 12), command = vvi.destroy)
-        welcome_message.grid(row = 0, column = 0)
-        backVVI.grid(row=1, column=0)
-        vvi_middleframe.pack()
-        vvi.mainloop()
 
     def open_VOO(self):
         voo = tk.Tk()
@@ -51,10 +42,29 @@ class modes():
         voo_middleframe = tk.Frame(voo, bg = '#4863A0')
         welcome_message = tk.Label(voo_middleframe, text = "VOO", bg='#4863A0', fg='#FFFFFF', font=("Arial", 16))
         backVOO = tk.Button(voo_middleframe, text = "Back", bg='#FFFFFF', fg='#000000', font=("Arial", 12), command = voo.destroy)
+        enableVOO = tk.Button(voo_middleframe, text = "Enable VOO", bg='#FFFFFF', fg='#000000', font=("Arial", 12) ) #implement serial enable 
         welcome_message.grid(row = 0, column = 0)
-        backVOO.grid(row = 1, column = 0)
+        enableVOO.grid(row = 1, column=0, pady=10)
+        backVOO.grid(row = 2, column = 0)
         voo_middleframe.pack()
         voo.mainloop()
+
+    def open_VVI(self):
+        vvi = tk.Tk()
+        vvi.geometry("500x400")
+        vvi.configure(bg='#4863A0')
+        vvi.title("VVI Page")
+        vvi_middleframe = tk.Frame(vvi, bg = '#4863A0')
+        welcome_message = tk.Label(vvi_middleframe, text = "VVI", bg='#4863A0', fg='#FFFFFF', font=("Arial", 16))
+        backVVI = tk.Button(vvi_middleframe, text = "Back", bg='#FFFFFF', fg='#000000', font=("Arial", 12), command = vvi.destroy)
+        enableVVI = tk.Button(vvi_middleframe, text = "Enable VVI", bg='#FFFFFF', fg='#000000', font=("Arial", 12) ) #implement serial enable 
+        welcome_message.grid(row = 0, column = 0)
+        enableVVI.grid(row = 1, column=0, pady=10)
+        backVVI.grid(row = 2, column = 0)
+        vvi_middleframe.pack()
+        vvi.mainloop()
+
+   
 
     def open_AOOR(self): 
         AOOR = tk.Tk()
