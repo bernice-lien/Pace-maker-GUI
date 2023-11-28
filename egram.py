d975ba594dad3315a7c5536bb3b051757ee9a04f
Alexandra Atkinson <atkina10@mcmaster.ca>
2023-11-28 13:38:41 -0500

    makes graph with random values

--- egram.py
+++ egram.py
@@ -1,4 +1,4 @@
-import random
+""" import random
 from itertools import count
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,9 +8,11 @@ import tkinter as tk
 import csv
 import random
 import time
+from threading import *
 
 class egram():
-    def graph(self):
+
+    def window(self):
         graph_window = Tk()
         graph_window.title("Egram Data")
         graph_window.geometry("400x400")
@@ -78,3 +80,84 @@ class egram():
 
             time.sleep(1)
 
+    T = Thread(target = data_gen)
+    #T.setDaemon(True)
+    T.start()
+    window()
+
+ """
+
+import random
+from itertools import count
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from tkinter import *
+import csv
+import random
+import time
+from threading import *
+
+def window():
+    root = Tk()
+    root.title("hello")
+    root.geometry("400x200")
+
+    plt.style.use('fivethirtyeight')
+
+    x_vals = []
+    y_vals = []
+
+    index = count()
+
+    def graph():
+
+        def animate(i):
+            data = pd.read_csv('data.csv')
+            x = data['x_value']
+            y = data['amplitude']
+
+            plt.cla()
+            plt.plot(x, y, label='Amplitude')
+            plt.tight_layout()
+
+        ani = FuncAnimation(plt.gcf(), animate, interval=100, cache_frame_data=False)
+
+        plt.tight_layout()
+        plt.show()
+
+    button = Button(root, text="Graph It!", command=graph)
+    button.pack()
+    root.mainloop()
+
+def data_gen():
+    x_value = 0
+    amplitude = 1000
+
+    fieldnames = ["x_value", "amplitude"]
+
+    with open('data.csv', 'w') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+
+    while True:
+        with open('data.csv', 'a') as csv_file:
+            csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            info = {
+                "x_value": x_value,
+                "amplitude": amplitude,
+            }
+
+            csv_writer.writerow(info)
+            print(x_value, amplitude)
+
+            x_value += 1
+            amplitude = amplitude + random.randint(-6, 8) #will come from serial communication
+
+        time.sleep(1)
+
+T = Thread(target = data_gen)
+T.setDaemon(True)
+T.start()
+window()
